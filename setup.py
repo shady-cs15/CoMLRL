b5b62c88dfb81de317e604f68f03b00d5512bfde
N!no <ninomyemail@gmail.com>
2025-11-15 13:24:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 # Core dependencies (excluding PyTorch which should be installed separately)
@@ -32,6 +34,18 @@ extras_require = {
     "all": full_requires + dev_requires,
 }
 
+short_description = (
+    "CoMLRL trains multiple LLMs to collaborate via cooperative reinforcement "
+    "learning algorithms."
+)
+
+long_description = short_description + (
+    "\nCoMLRL is an open-source library for training multiple LLMs to "
+    "collaborate using Multi-Agent Reinforcement Learning (MARL). It provides "
+    "implementations of various MARL algorithms for LLM collaboration and support "
+    "for different environments and benchmarks."
+)
+
 setup(
     name="comlrl",
     use_scm_version=True,
@@ -40,4 +54,7 @@ setup(
     python_requires=">=3.8",
     install_requires=core_requires,
     extras_require=extras_require,
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
